dbd31f1fdfbb9bf2d82055056dcc77a00be8be5d
guilhermamorim <guilhermeamorim@deso-se.com.br>
2021-12-19 11:39:06 -0300

    Atualização: criação do presente com laços

---
+++ components/Presente.jsx
@@ -0,0 +1,20 @@
+import styles from "../styles/Presente.module.css"
+
+export default function Presente() {
+    return (
+        <div className={styles.presente}>
+            <div className={styles.tampa}>
+                
+            </div>
+            <div className={styles.corpo}>
+                
+            </div>
+            <div className={styles.laco_1}>
+                
+            </div>
+            <div className={styles.laco_2}>
+                
+            </div>
+        </div>
+    )
+}
