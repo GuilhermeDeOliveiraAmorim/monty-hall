337a4c489b78a0c26df056a9d08d46b1197e7f9d
GuilhermeDeOliveiraAmorim <guilhermeamorim@deso-se.com.br>
2021-12-21 13:59:39 -0300

    Atualização: porta

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,16 @@
 import Porta from "../components/Porta";
+import PortaModel from "../model/porta";
 
 
 export default function Home() {
+
+    const p1 = new PortaModel(1, false, true);
+    const p2 = new PortaModel(2);
+
     return (
         <div style={{display:"flex"}}>
-            <Porta />
-            <Porta />
+            <Porta porta={p1} />
+            <Porta porta={p2} />
         </div>
     )
 }
