66884a7d0f13abbd5ced594d9b7f2544bd625898
GuilhermeDeOliveiraAmorim <guilhermeamorim@deso-se.com.br>
2021-12-22 13:40:23 -0300

    Atualização: lógica para as portas

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Porta from "../components/Porta";
+import { atualizarPortas, criarPortas } from "../functions/portas";
+import PortaModel from "../model/porta";
+
+export default function Home() {
+
+    const [p1, setP1] = useState(new PortaModel(1));
+
+    const [portas, setPortas] = useState(criarPortas(3, 2));
+
+    function renderizarPortas() {
+        return portas.map(porta => {
+            return <Porta key={porta.numero} value={porta} onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}/>;
+        })
+    }
+
+    return (
+        <div style={{display:"flex"}}>
+            {renderizarPortas()}
+        </div>
+    )
+}
